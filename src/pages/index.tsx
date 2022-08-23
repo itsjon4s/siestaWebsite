beb55f70b46b3664ce396b98e4bef68fb0bc698f
itsjon4s <motajoao670@gmail.com>
2022-08-23 14:23:59 +0100

    fix: lilte size changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,14 @@ const Home: NextPage = () => {
         <meta name="description" content="Siesta, a multipropuse discord bot, music focused with over 200 servers!" />
       </Head>
       <NavBar />
-      <div className="bg-[#3d3e51] h-1 mt-3"></div>
-      <div className="my-20 flex flex-col md:flex-row items-center justify-center">
-        <Image src="/siesta.jpeg" width="350px" height="350px" className="rounded-full shadow-md" />
+      <div className="bg-[#3d3e51] h-1 m-6"></div>
+      <div className="my-20 flex flex-col md:flex-row items-center justify-center space-y-6">
+        <Image src="/siesta.jpeg" width="300px" height="300px" className="rounded-full shadow-md" />
         <div className="flex flex-col">
-          <h1 className="font-semibold md:ml-20 text-center text-6xl">
+          <h1 className="font-semibold text-center text-6xl">
             Its time to invite <strong>Siesta</strong>!
           </h1>
-          <div className="text-3xl font-light md:ml-20 text-center pt-5">
+          <div className="text-3xl font-light md:ml-10 text-center pt-5">
             <p>
               My name is <strong className="font-bold">Siesta</strong> a <strong className="font-bold">multifunctional discord bot</strong>{' '}
             </p>
