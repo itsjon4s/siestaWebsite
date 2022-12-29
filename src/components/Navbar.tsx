bee35d866e91246c579a862ba0dcec425919ca4d
itsjon4s <88103711+itsjon4s@users.noreply.github.com>
2022-12-29 19:48:49 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,9 +19,6 @@ const Navbar = () => {
               <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                   <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <Image height="70" width="70" src="/siesta.jpeg" className="rounded-full" />
-                    </div>
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map(([name, href], index) => (
